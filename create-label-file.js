2208de2e4f646b56ee72d00dee09e9cd809221d2
gignupg <linus.komnick@googlemail.com>
2020-12-16 19:35:29 +0100

    Possibly the first fully working version

--- create-label-file.js
+++ create-label-file.js
@@ -1,14 +1,35 @@
 import fs from 'fs';
-import { parse, map } from 'subtitle';
+import { parse, map, filter } from 'subtitle';
+
+let previousEnd = 0;
 
 fs.createReadStream('./seul-sur-mars.srt', 'latin1')
   .pipe(parse())
-  .pipe(map((node) => {
+  .pipe(map((node, index) => {
     if (node.type === 'cue') {
+      const prevEnd = previousEnd;
       const elem = node.data;
-      const text = elem.text.replace(/\n/g, " ");
 
-      return `${elem.start}\t\t${elem.end}\t\t${text}\n`;
+      const end = numberConverter(elem.end);
+      const start = numberConverter(elem.start)
+
+      previousEnd = end; 
+
+      if (prevEnd || start > 2) {
+        if (start - prevEnd > 2) {
+          return `${prevEnd}\t\t${start}\t\tSilence\n`;
+        }
+      }
+      return null;
     }
   }))
+  .pipe(filter((elem) => {
+    return elem;
+  }))
   .pipe(fs.createWriteStream('./my-subtitles.txt', 'latin1'));
+
+  
+  function numberConverter(num) {
+    num = num.toString();
+    return Number(num.slice(0, num.length - 3) + "." + num.slice(num.length - 3));
+  }
