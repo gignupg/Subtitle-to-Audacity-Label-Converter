9bce8e437f5a22db5bc0e974c77270dfadca1627
gignupg <linus.komnick@googlemail.com>
2020-12-22 14:45:13 +0100

    Making it more generic. Now it can be easily used from the command line, e.g. with an alias

--- create-label-file.js
+++ create-label-file.js
@@ -1,35 +1,62 @@
 import fs from 'fs';
+import os from 'os';
+import path from 'path';
 import { parse, map, filter } from 'subtitle';
 
-let previousEnd = 0;
+const homedir = os.homedir();
 
-fs.createReadStream('./boca-norte-1.srt', 'latin1')
-  .pipe(parse())
-  .pipe(map(node => {
-    if (node.type === 'cue') {
-      const prevEnd = previousEnd;
-      const elem = node.data;
+const downloadDir = path.join(homedir, '/Downloads');
 
-      const end = numberConverter(elem.end);
-      const start = numberConverter(elem.start)
+fs.readdir(downloadDir, function (err, files) {
+  if (err) {
+    console.log(err);
 
-      previousEnd = end; 
+  } else {
+    const srtFiles = files.filter(el => path.extname(el).toLowerCase() === ".srt");
 
-      if (prevEnd || start > 2) {
-        if (start - prevEnd > 2) {
-          return `${prevEnd + 0.2}\t\t${start - 0.2}\t\tS\n`;
-        }
-      }
-      return null;
+    if (srtFiles && srtFiles.length === 1) {
+      const fileName = path.join(downloadDir, srtFiles[0]);
+
+      let previousEnd = 0;
+
+      fs.createReadStream(fileName, 'latin1')
+        .pipe(parse())
+        .pipe(map(node => {
+          if (node.type === 'cue') {
+            const prevEnd = previousEnd;
+            const elem = node.data;
+    
+            const end = numberConverter(elem.end);
+            const start = numberConverter(elem.start);
+    
+            previousEnd = end;
+    
+            if (prevEnd || start > 2) {
+              if (start - prevEnd > 2) {
+                return `${prevEnd + 0.9}\t\t${start - 0.9}\t\tS\n`;
+              }
+            }
+            return null;
+          }
+        }))
+        .pipe(filter((elem) => {
+          return elem;
+        }))
+        .pipe(fs.createWriteStream(`${downloadDir}/new-audacity-label.txt`, 'latin1'));
+    
+    } else {
+      console.log("Conversion failed. Make sure you are in the Downloads folder and there is no more than one srt file present!")
     }
-  }))
-  .pipe(filter((elem) => {
-    return elem;
-  }))
-  .pipe(fs.createWriteStream('./new-audacity-label.txt', 'latin1'));
-
-
-  function numberConverter(num) {
-    num = num.toString();
-    return Number(num.slice(0, num.length - 3) + "." + num.slice(num.length - 3));
   }
+});
+
+
+function numberConverter(num) {
+  num = num.toString();
+  return Number(num.slice(0, num.length - 3) + "." + num.slice(num.length - 3));
+}
+
+
+
+
+
