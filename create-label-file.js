54692a0ab761f78644dfe64ee9d96a7955fb4504
gignupg <linus.komnick@googlemail.com>
2020-12-24 10:53:16 +0100

    Adding ability to distinguish between utf-8 and latin1

--- create-label-file.js
+++ create-label-file.js
@@ -2,6 +2,12 @@ import fs from 'fs';
 import os from 'os';
 import path from 'path';
 import { parse, map, filter } from 'subtitle';
+import detectCharacterEncoding from 'detect-character-encoding';
+
+const encodingTable = {
+  "ISO-8859-1": "latin1",
+  "UTF-8": "utf8"
+}
 
 const homedir = os.homedir();
 
@@ -9,7 +15,7 @@ const downloadDir = path.join(homedir, '/Downloads');
 
 fs.readdir(downloadDir, function (err, files) {
   if (err) {
-    console.log(err);
+    console.log("File cannot be properly processed for the following reason:", err);
 
   } else {
     const srtFiles = files.filter(el => path.extname(el).toLowerCase() === ".srt");
@@ -17,20 +23,24 @@ fs.readdir(downloadDir, function (err, files) {
     if (srtFiles && srtFiles.length === 1) {
       const fileName = path.join(downloadDir, srtFiles[0]);
 
+      // Encoding
+      const fileBuffer = fs.readFileSync(fileName);
+      const fileEncoding = detectCharacterEncoding(fileBuffer);
+      
       let previousEnd = 0;
 
-      fs.createReadStream(fileName, 'latin1')
+      fs.createReadStream(fileName, encodingTable[fileEncoding])
         .pipe(parse())
         .pipe(map(node => {
           if (node.type === 'cue') {
             const prevEnd = previousEnd;
             const elem = node.data;
-    
+
             const end = numberConverter(elem.end);
             const start = numberConverter(elem.start);
-    
+
             previousEnd = end;
-    
+
             if (prevEnd || start > 2) {
               if (start - prevEnd > 2) {
                 return `${prevEnd + 0.9}\t\t${start - 0.9}\t\tS\n`;
@@ -42,10 +52,10 @@ fs.readdir(downloadDir, function (err, files) {
         .pipe(filter((elem) => {
           return elem;
         }))
-        .pipe(fs.createWriteStream(`${downloadDir}/new-audacity-label.txt`, 'latin1'));
-    
+        .pipe(fs.createWriteStream(`${downloadDir}/new-audacity-label.txt`, encodingTable[fileEncoding]));
+
     } else {
-      console.log("Conversion failed. Make sure you are in the Downloads folder and there is no more than one srt file present!")
+      console.log("Conversion failed. Make sure you are in the Downloads folder and there is no more than one srt file present!");
     }
   }
 });
@@ -55,8 +65,3 @@ function numberConverter(num) {
   num = num.toString();
   return Number(num.slice(0, num.length - 3) + "." + num.slice(num.length - 3));
 }
-
-
-
-
-
